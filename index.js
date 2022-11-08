b84a1f84a7686737660440e04556bdb5dbec7f15
Lucas José <lukinhas8979@gmail.com>
2022-11-08 18:53:59 -0300

    subindo todo o progresso ate entao

---
+++ index.js
@@ -0,0 +1,47 @@
+const maxPokemon = 649
+let idNow = 1
+
+
+const buscar = async (name) => {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
+    if (data.ok == true) return data.json()
+    }
+
+
+const imgFront = document.querySelector('.img-front')
+const imgBack = document.querySelector('.img-back')
+
+const render = async (name) => {
+    const data = await buscar(name)
+    idNow = data.id
+    input.value = ''
+    
+    imgFront.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+    imgBack.src = data['sprites']['versions']['generation-v']['black-white']['animated']['back_default'] 
+    }
+
+
+const form = document.querySelector('.form')
+const input = document.querySelector('.input')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    render(input.value)
+    })
+
+
+const buttonLeft = document.querySelector('.button-left')
+const buttonRight = document.querySelector('.button-right')
+
+buttonLeft.addEventListener('click', () => {
+    if (idNow > 1) render(String(idNow-1))
+    else render(maxPokemon.toString())
+    })
+
+buttonRight.addEventListener('click', () => {
+    if (idNow < maxPokemon) render(String(idNow+1))
+    else render('1')
+    })
+
+
+render(idNow.toString())
